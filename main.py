672daae989a325596ac54013b81ae3d20f456e67
Paul Becker <s180124@th-ab.de>
2020-11-06 19:23:47 +0100

    added lowlevel program

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
-import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
+import programs.lowlevel as lowlevel
+
 app = Flask(__name__)
-
-GPIO.setmode(GPIO.BCM)
-
-# Create a dictionary called pins to store the pin number, name, and pin state:
-pins = {
-   23 : {'name' : 'GPIO 23', 'state' : GPIO.LOW},
-   24 : {'name' : 'GPIO 24', 'state' : GPIO.LOW}
-   }
-
-# Set each pin as an output and make it low:
-for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
 
 @app.route("/")
 def main():
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-   # Put the pin dictionary into the template data dictionary:
-   templateData = {
-      'pins' : pins
-      }
-   # Pass the template data into the template main.html and return it to the user
-   return render_template('main.html', **templateData)
-
-# The function below is executed when someone requests a URL with the pin number and action in it:
-@app.route("/on")
-def action():
-    
     return render_template('main.html')
 
-
 @app.route("/on")
 def action():
+    lowlevel.lowlevel()
     return render_template('main.html')
 
 if __name__ == "__main__":
