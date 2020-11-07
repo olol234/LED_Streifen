eb19ade53c6622af049b34dbf533639cf0c88bae
Paul Becker <s180124@th-ab.de>
2020-11-07 16:51:39 +0100

    test

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def action():
 
 @app.route("/action_page.php")
 def action1():
-    x = request.from['#ff0000']
+    x = request.from['favcolor']
     led.colorpicker(x)
     return render_template('main.html')
 
