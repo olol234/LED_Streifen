ea4dc1d5c2cbdcf701980db73a780fc1253d9194
Paul Becker <s180124@th-ab.de>
2020-11-07 13:44:42 +0100

    added led-control

--- programs/led_control.py
+++ programs/led_control.py
@@ -1,7 +1,7 @@
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D18, 30)
+pixels = neopixel.NeoPixel(board.D18, 109)
 
-def green():
-    pixels.fill((0, 255, 0))
+def green(color):
+    pixels.fill((0,255,0))
