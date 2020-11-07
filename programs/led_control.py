52c2cd4c0825334fec06ec2de0cf3e2e24af298a
Paul Becker <s180124@th-ab.de>
2020-11-07 15:29:21 +0100

    test

--- programs/led_control.py
+++ programs/led_control.py
@@ -6,5 +6,5 @@ pixels = neopixel.NeoPixel(board.D18, 109)
 def green():
     pixels.fill((0,255,0))
 
-def colorpicker(favcolor):
-    pixels.fill((favcolor))
+def colorpicker(color):
+    pixels.fill((color))
