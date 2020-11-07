6000f2a047aaa08eebf8765debee8bed9d7cb996
Paul Becker <s180124@th-ab.de>
2020-11-07 17:03:28 +0100

    test

--- programs/led_control.py
+++ programs/led_control.py
@@ -1,8 +1,5 @@
 import board
 import neopixel
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
 
 pixels = neopixel.NeoPixel(board.D18, 109)
 
@@ -10,5 +7,4 @@ def green():
     pixels.fill((0,255,0))
 
 def colorpicker():
-    x = request.form['#ff0000']
     pixels.fill((0,0,0))
