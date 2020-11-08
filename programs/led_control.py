4b4fc4af64aed36b4013c8ee00211cdc9941982f
Paul Becker <s180124@th-ab.de>
2020-11-08 23:21:42 +0100

    Wheel added

--- programs/led_control.py
+++ programs/led_control.py
@@ -21,7 +21,7 @@ def wheel(pos):
     # Input a value 0 to 255 to get a color value.
     # The colours are a transition r - g - b - back to r.
     if pos < 0 or pos > 255:
-        pixels.fill(0, 0, 0)
+        pixels.fill((0, 0, 0))
     if pos < 85:
         pixels.fill(255 - pos * 3, pos * 3, 0)
     if pos < 170:
