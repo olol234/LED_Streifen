ad665eee54eaf1e0a1252b2c9740aa016a017859
Paul Becker <s180124@th-ab.de>
2020-11-08 23:22:27 +0100

    Wheel added

--- programs/led_control.py
+++ programs/led_control.py
@@ -23,10 +23,10 @@ def wheel(pos):
     if pos < 0 or pos > 255:
         pixels.fill((0, 0, 0))
     if pos < 85:
-        pixels.fill(255 - pos * 3, pos * 3, 0)
+        pixels.fill((255 - pos * 3, pos * 3, 0))
     if pos < 170:
         pos -= 85
-        pixels.fill(0, 255 - pos * 3, pos * 3)
+        pixels.fill((0, 255 - pos * 3, pos * 3))
 
     pos -= 170
-    pixels.fill(pos * 3, 0, 255 - pos * 3)
+    pixels.fill((pos * 3, 0, 255 - pos * 3))
