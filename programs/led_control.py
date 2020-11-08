d18b5acf9ac442876767adb914235b4a27e77c23
Paul Becker <s180124@th-ab.de>
2020-11-08 21:15:10 +0100

    Wheel added

--- programs/led_control.py
+++ programs/led_control.py
@@ -19,12 +19,11 @@ def colorpicker(color_string):
     pixels.fill(tuple(int(color_string[i:i + hlen // 3], 16) for i in range(0, hlen, hlen // 3)))
 
 def wheel():
-	"""Generate rainbow colors across 0-255 positions."""
 	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
+		pixels.fill(pos * 3, 255 - pos * 3, 0)
 	elif pos < 170:
 		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
+		pixels.fill(255 - pos * 3, 0, pos * 3)
 	else:
 		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
+		pixels.fill(0, pos * 3, 255 - pos * 3)
