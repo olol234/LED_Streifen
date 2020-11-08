ebab8169f095b0ae39881c8a2f478ab1223bf7eb
Paul Becker <s180124@th-ab.de>
2020-11-08 20:39:58 +0100

    Wheel added

--- main.py
+++ main.py
@@ -19,11 +19,6 @@ def action2():
     led.off()
     return render_template('main.html')
 
-@app.route("/wheel")
-def action2():
-    led.wheel()
-    return render_template('main.html')
-
 @app.route("/colorpicker", methods=['GET','POST'])
 def action1():
     color_string = request.form['favcolor']
--- programs/led_control.py
+++ programs/led_control.py
@@ -21,25 +21,3 @@ def colorpicker(color_string):
     pixels.fill(tuple(int(color_string[i:i + hlen // 3], 16) for i in range(0, hlen, hlen // 3)))
 
 
-def wheel():
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos * 3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos * 3)
-        g = 0
-        b = int(pos * 3)
-    else:
-        pos -= 170
-        r = 0
-        g = int(pos * 3)
-        b = int(255 - pos * 3)
-    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
-
-
