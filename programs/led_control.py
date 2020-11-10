4c685913bdbcf81110bbc84bfd74cba10771c08d
Paul Becker <s180124@th-ab.de>
2020-11-10 22:31:45 +0100

    random color added

--- programs/led_control.py
+++ programs/led_control.py
@@ -20,7 +20,6 @@ def random():
 
 def off():
     global wheel_execute
-    global rainbow_execute
     wheel_execute = False
     pixels.fill((0,0,0))
 
@@ -53,55 +52,3 @@ def wheel():
         x.start()
 
 
-def color_chase(color, wait):
-    for i in range(num_pixels):
-        pixels[i] = color
-        time.sleep(wait)
-        pixels.show()
-    time.sleep(0.5)
-
-def rainbow():
-    global rainbow_execute
-    if not rainbow_execute:
-        rainbow_execute = True
-        x = threading.Thread(target=rainbow_cycle, args=())
-        x.start()
-
-def rainbow_cycle(wait):
-    RED = (255, 0, 0)
-    YELLOW = (255, 150, 0)
-    GREEN = (0, 255, 0)
-    CYAN = (0, 255, 255)
-    BLUE = (0, 0, 255)
-    PURPLE = (180, 0, 255)
-
-    global rainbow_execute
-    while (rainbow_execute):
-     for j in range(255):
-        for i in range(num_pixels):
-            rc_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(rc_index & 255)
-        pixels.show()
-        time.sleep(wait
-
-
-while True:
-    pixels.fill(RED)
-    pixels.show()
-    # Increase or decrease to change the speed of the solid color change.
-    time.sleep(1)
-    pixels.fill(GREEN)
-    pixels.show()
-    time.sleep(1)
-    pixels.fill(BLUE)
-    pixels.show()
-    time.sleep(1)
-
-    color_chase(RED, 0.1)  # Increase the number to slow down the color chase
-    color_chase(YELLOW, 0.1)
-    color_chase(GREEN, 0.1)
-    color_chase(CYAN, 0.1)
-    color_chase(BLUE, 0.1)
-    color_chase(PURPLE, 0.1)
-
-    rainbow_cycle(0)  # Increa
