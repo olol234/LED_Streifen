5b0af0650667b740c9c4f31fec974b6a035bcb72
Paul Becker <s180124@th-ab.de>
2020-11-10 22:23:55 +0100

    random color added

--- programs/led_control.py
+++ programs/led_control.py
@@ -69,7 +69,7 @@ def rainbow():
 
 def rainbow_cycle(wait):
     global rainbow_execute
-    while (rainbow_execute)
+    while (rainbow_execute):
      for j in range(255):
         for i in range(num_pixels):
             rc_index = (i * 256 // num_pixels) + j
