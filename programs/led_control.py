0be3b8b44944468d3153c7314843275ccf576ab7
Paul Becker <s180124@th-ab.de>
2020-11-15 19:03:28 +0100

    thearterchase added

--- programs/led_control.py
+++ programs/led_control.py
@@ -9,22 +9,24 @@ pixels = neopixel.NeoPixel(board.D18, num_pixels,auto_write = False)
 
 wheel_execute = False
 fadergb_execute = False
-theaterchase_ecexute = False
+theaterchase_execute = False
 
 def green():
     pixels.fill((9,138,236))
+    pixels.show()
 
 
 def off():
     pixels.fill((0,0,0))
+    pixels.show()
 
 def reset():
     global wheel_execute
     global fadergb_execute
-    global theaterchase_ecexute
+    global theaterchase_execute
     wheel_execute = False
     fadergb_execute = False
-    theaterchase_ecexute = False
+    theaterchase_execute = False
     pixels.fill((0,0,0))
     pixels.show()
 
@@ -33,8 +35,9 @@ def colorpicker(color_string):
     color_string = color_string.lstrip('#')
     hlen = len(color_string)
     pixels.fill(tuple(int(color_string[i:i + hlen // 3], 16) for i in range(0, hlen, hlen // 3)))
+    pixels.show()
 
-def wheel_thread():
+def wheel_thread(wait=1):
     pos = 0
     global wheel_execute
     while (wheel_execute):
@@ -48,6 +51,8 @@ def wheel_thread():
         else:
             pixels.fill((0, pos, 255 - pos ))
             pos -= 90
+    pixels.show()
+    time.sleep(wait)
 
 def wheel():
     global wheel_execute
@@ -69,6 +74,7 @@ def fadergb_thread():
                     pixels.fill((0, j, 0))
                 elif i == 2:
                     pixels.fill((0, 0, j))
+                pixels.show()
 
             for j in range(255, 0, -1):
                 if i == 0:
@@ -77,7 +83,7 @@ def fadergb_thread():
                     pixels.fill((0, j, 0))
                 elif i == 2:
                     pixels.fill((0, 0, j))
-
+                pixels.show()
 
 def fadergb():
     global fadergb_execute
@@ -87,15 +93,15 @@ def fadergb():
         x.start()
 
 def theaterchase():
-    global theaterchase_ecexute
-    if not theaterchase_ecexute:
-        theaterchase_ecexute = True
+    global theaterchase_execute
+    if not theaterchase_execute:
+        theaterchase_execute = True
         x = threading.Thread(target=theaterchase_thread, args=())
         x.start()
 
 def theaterchase_thread(wait=0.3):
-    global theaterchase_ecexute
-    while (theaterchase_ecexute):
+    global theaterchase_execute
+    while (theaterchase_execute):
         for j in range(0,255,30):
             for k in range(0,3):
                 for i in range(num_pixels-k):
