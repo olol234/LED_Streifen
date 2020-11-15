f187a5e61bd21e56495d77b02372066659f2145c
Paul Becker <s180124@th-ab.de>
2020-11-15 16:07:43 +0100

    thearterchase added

--- programs/led_control.py
+++ programs/led_control.py
@@ -100,6 +100,6 @@ def theaterchase_thread():
         for i in range(0, 3):
             for k in range(0, num_pixels, 3):
                 pixels[k+1] = (255,0,0)
-                time.sleep(3)
+                time.sleep(0.1)
             for k in range(0, num_pixels, 3):
                 pixels[k+1] = (0,0,0)
