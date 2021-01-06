a29f3f3c55ab5d5aae6c4814f29f52c73de0e428
Paul Becker <s180124@th-ab.de>
2021-01-06 15:19:28 +0100

    styeelsheet front page

--- main.py
+++ main.py
@@ -12,56 +12,56 @@ def main():
 @app.route("/test")
 def test():
     led.test()
-    return render_template('main.html')
+    return render_template('licht.html')
 
 
 @app.route("/on")
 def on():
     led.green()
-    return render_template('main.html')
+    return render_template('licht.html')
 
 
 @app.route("/off")
 def off():
     led.off()
-    return render_template('main.html')
+    return render_template('licht.html')
 
 
 @app.route("/reset")
 def reset():
     led.reset()
-    return render_template('main.html')
+    return render_template('licht.html')
 
 
 @app.route("/random")
 def random():
     led.random()
-    return render_template('main.html')
+    return render_template('licht.html')
 
 
 @app.route("/Wheel")
 def Wheel():
     led.wheel()
-    return render_template('main.html')
+    return render_template('licht.html')
 
 
 @app.route("/fadeRGB")
 def fadeRGB():
     led.fadergb()
-    return render_template('main.html')
+    return render_template('licht.html')
 
 
 @app.route("/theaterChase")
 def theaterChase():
     led.theaterchase()
-    return render_template('main.html')
+    return render_template('licht.html')
 
 
 @app.route("/colorpicker", methods=['GET', 'POST'])
 def colorpicker():
     color_string = request.form['favcolor']
     led.colorpicker(color_string)
-    return render_template('main.html')
+    return render_template('licht.html')
 
 
 @app.route("/licht")
